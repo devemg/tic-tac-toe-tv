8a9034b9b83358b1dbaee4668e3c3d88c418f733
Emely Garcia <garciam.emm@gmail.com>
2025-06-15 11:22:17 -0600

    feat: add message on clear action

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,28 +1,51 @@
 import { useNavigate } from 'react-router';
 import styles from './settings.module.scss';
 import backIcon from '@assets/arrow-left.svg';
-import { useEffect, useRef } from 'react';
+import infoIcon from '@assets/info.svg';
+import { useEffect, useRef, useState } from 'react';
 import { focusContainerRef } from '@utils/focus.utils';
 import { clearGames, clearNames } from '@store/Store';
+import { clsx } from 'clsx';
 
 export const SettingsPage = () => {
   const navigate = useNavigate();
   const optionsRef = useRef(null);
-
+  const [showInfoMessage, setShowInfoMessage] = useState<boolean>();
+  const [infoMessage, setInfoMessage] = useState<string>('');
   useEffect(() => {
     focusContainerRef(optionsRef);
   }, []);
 
   const clearPlayers = () => {
     clearNames();
+    showMessage('The player suggestions has been removed.');
   }
 
   const clearHistory = () => {
     clearGames();
+    showMessage('The player history has been removed.');
+  }
+
+  const showMessage = (message: string) => {
+    setShowInfoMessage(true);
+    setInfoMessage(message);
+    setTimeout(() => {
+      setShowInfoMessage(false);
+    }, 3000);
+
   }
 
   return (
     <div className={styles['page']}>
+
+      <div className={clsx(
+        styles['page-info'],
+        showInfoMessage ? styles['show'] : styles['hide']
+      )}>
+        <img src={infoIcon} alt="Info" />
+        <p>{infoMessage}</p>
+      </div>
+
       <h2 className="page-header">
         <img src={backIcon} alt="Left Arrow" onClick={() => navigate('/')} />
       </h2>
